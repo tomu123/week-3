ef082549800ff732df4c1d8318086f61db5a8605
Tomu Komatsu <tomu5642514@gmail.com>
2022-03-21 09:49:52 -0500

    Test Validations

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,11 @@
-require "test_helper"
+require 'test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'should not save customer without name' do
+    customer = Customer.new(last_name: 'Komatsu', address: 'JP', phone: '5642514')
+    assert_not customer.save, 'Saved the customer without a name'
+  end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,15 @@
-require "test_helper"
+require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'should not save product without name' do
+    product = Product.new(sku: 'u2u3', price: 30, stock: 40)
+    assert_not product.save, 'Saved the product without a name'
+  end
+
+  test 'should not update product without price' do
+    assert_not Product.first.update(price: nil), 'Saved the product without a price'
+  end
 end
